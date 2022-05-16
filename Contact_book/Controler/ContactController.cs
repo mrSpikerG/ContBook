dd3ee88151bdbdb5bf6a2d8fb15e485f65a03e36
Yana Doll <Dolz_op79@student.itstep.org>
2022-05-16 21:06:12 +0400

    cotact controlls, contact

---
+++ Contact_book/Controler/ContactController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_book.Controler
+{
+    class ContactController
+    {
+        public List<Contact> list_contacts { get; set; }
+
+        public ContactController()
+        {
+            list_contacts = new List<Contact>();
+            this.list_contacts.Add(new Contact("Ponomarenko", "Artem", "Mikhailovich", new List<string> { "+38067674556633" },"Ivanovskaya,22"));
+            this.list_contacts.Add(new Contact("Azarova", "Karolina", "Andreevna", new List<string> { "+38067004556633" }, "Shmidta,35"));
+            this.list_contacts.Add(new Contact("Shut", "Mila", "Oleksandrovna", new List<string> { "+38063688556633" }, "Korolenka,22"));
+        }
+
+
+        public void AddContact(Contact item)
+        {
+            this.list_contacts.Add(item);
+
+        }
+
+        public void DeleteContact(Contact item)
+        {
+            if(this.list_contacts.Contains(item))
+            this.list_contacts.Remove(item);
+
+        }
+        public List<Contact> GetContact()
+        {
+            return list_contacts;
+
+        }
+
+    }
+}
---
+++ Contact_book/Model/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_book
+{
+    class Contact
+    {
+        
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Father_Name { get; set; }
+        public List<string> phone_number { get; set; }
+        public string adress { get; set; }
+
+
+        public Contact( string surname, string name, string father_Name, List<string> phone_number, string adress)
+        {
+            Name = name;
+            Surname = surname;
+            Father_Name = father_Name;
+            this.phone_number = phone_number;
+            this.adress = adress;
+        }
+
+
+
+    }
+}
